77ddf253f57859afca19ab85679629e16beccf66
Wajahat Qasim <wajahat_um99@yahoo.com>
2016-01-18 11:38:09 +0500

    Structure

--- application/views/admin/layout/footer.php
+++ application/views/admin/layout/footer.php
@@ -5,7 +5,14 @@
     
     <script src="<?php echo base_url('public/admin/js/jquery-ui.js') ?>"></script>
     <script src="<?php echo base_url('public/admin/js/admin-scripts.js') ?>"></script>
-    
-   
+
+    <script src="<?php echo base_url('public/admin/js/fabric.min.js') ?>"></script> 
+    <script src="<?php echo base_url('public/admin/js/fabric/fabric_api.js') ?>"></script> 
+    <script src="<?php echo base_url('public/admin/js/script.js') ?>"></script>
+    <script src="<?php echo base_url('public/admin/js/fabric/text.js') ?>"></script>
+    <script src="<?php echo base_url('public/admin/js/fabric/image.js') ?>"></script>
+    <script src="<?php echo base_url('public/admin/js/fabric/shapes.js') ?>"></script>
+    <script src="<?php echo base_url('public/admin/js/fabric/shapes.js') ?>"></script>
+
 </body>
 </html>
