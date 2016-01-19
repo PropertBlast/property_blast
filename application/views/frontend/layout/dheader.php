b5f9823bbe1eb2061aafc307dc3d732e94dcec9d
ahmadshoaib <ahmadshoaib@pakipreneurs.com>
2016-01-19 19:46:47 +0500

    GUI Setup and Errors resolved.

--- application/views/frontend/layout/dheader.php
+++ application/views/frontend/layout/dheader.php
@@ -10,11 +10,10 @@
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
         <!-- header css & js resources -->
-        <link rel="stylesheet" href="<?php echo base_url() ?>public/frontend/css/bootstrap.css">
-        <link rel="stylesheet" href="<?php echo base_url() ?>public/frontend/css/bootstrap-lightbox.min.css">
         <link rel="stylesheet" href="<?php echo base_url() ?>public/frontend//css/font-awesome.css">
         <link rel="stylesheet" href="<?php echo base_url() ?>public/frontend/css/master.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="<?php echo base_url() ?>public/frontend/css/style.css">
+        <link rel="stylesheet" href="<?php echo base_url() ?>public/frontend/css/bootstrap.css">
+        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
     </head>
     <body>
-<script> var base_url = '<?php echo base_url(); ?>' ;</script>
