cce901ffff023641cbf7357555669b9af5039026
Syed Tausif Ali Shah <engr.tausif@yahoo.com>
2016-01-31 12:09:02 -0800

    upload flyer completed.
    Signed-off-by: Syed Tausif Ali Shah <engr.tausif@yahoo.com>

--- application/controllers/admin/manageflyers.php
+++ application/controllers/admin/manageflyers.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
@@ -16,7 +17,6 @@ class manageflyers extends CI_Controller {
         }
         $this->load->model(array('admin/flyersmodel', 'frontend/commonmodel'));
         $this->load->library('form_validation');
-        
     }
 
     /**
@@ -27,58 +27,89 @@ class manageflyers extends CI_Controller {
         $data['sucess'] = $this->session->flashdata('sucess');
         $this->commonmodel->adminloadLayout($data, 'admin/manageflyers/content');
     }
-    
+
     public function save() {
 
-        $data  = '';
-        $data['flyertags']         = $this->flyersmodel->getAllflyer_tags();
-        $data['buttontags']        = $this->flyersmodel->getAllbutton_tags();
-        if(isset($_POST['submit'])){
-            
+        $data = '';
+        $data['error'] = FALSE;
+        $data['success'] = FALSE;
+        $data['flyerSize'] = $this->flyersmodel->getAllFlyerSize('Active');
+        $data['flyertags'] = $this->flyersmodel->getAllflyer_tags();
+        $data['buttontags'] = $this->flyersmodel->getAllbutton_tags();
+        if (isset($_POST['submit'])) {
+            $post = $_POST;
+          
+            $config['upload_path'] = './public/upload/flyer_images/';
+            $config['allowed_types'] = 'jpg|png';
+            $config['encrypt_name'] = TRUE;
+
+            $this->load->library('upload', $config);
+
+            $this->form_validation->set_rules('flyer_size', 'Flyer Size', 'required');
+            $this->form_validation->set_rules('btn_tags', 'Button Tags', 'required');
+            $this->form_validation->set_rules('flyer_tags', 'Flyer Tag', 'required');
+
+            if (!$this->form_validation->run()) {
+                
+            } else {
+                if (!$this->upload->do_upload('fileinput')) {
+                    $data['error'] = array('error' => $this->upload->display_errors());
+                } else {
+                    $fileUpload = array('upload_data' => $this->upload->data());
+                    $flyerID = $this->flyersmodel->saveFlyer(array('image' => $fileUpload['upload_data']['file_name'], 'sizeid' => $post['flyer_size']));
+                    if(!$flyerID){
+                        $data['error']['insert'] = '<div class="alert alert-danger" role="Alert">Flyer could not be saved to database. Try again.</div>';
+                    }else{
+                        $this->flyersmodel->saveFlyerBtnRelation(array('flyerid'=> $flyerID, 'btn' => $post['btn_tags']));
+                        $this->flyersmodel->saveFlyerFlyertagsRelation(array('flyerid'=> $flyerID, 'flyer' => $post['flyer_tags']));
+                        $data['success'] = '<div class="alert alert-success" role="Alert">Flyer Added successfully</div>';
+                    }
+                }
+            }
         }
         $this->commonmodel->adminloadLayout($data, 'admin/manageflyers/save');
     }
+
     public function add_flyer_tags() {
 
-        $data  = '';
+        $data = '';
         if (isset($_POST['submit'])) {
 
             $this->form_validation->set_rules('flyer_tags_title', 'Tags Title', 'required');
             if ($this->form_validation->run() == FALSE) {
-               
+                
             } else { // no errors now to save the data
-                $flyer_tags_title        = $this->input->post('flyer_tags_title');
+                $flyer_tags_title = $this->input->post('flyer_tags_title');
                 $datas = array(
-                        'flyer_tags_title' => $flyer_tags_title,
-                        'flyer_tags_date' => date('Y-m-d h:i:s')
-                    );
-                     $this->db->insert('tbl_flyer_tags', $datas);
-                $data['sucess']  = 'Flyer Added successfully';
+                    'flyer_tags_title' => $flyer_tags_title,
+                    'flyer_tags_date' => date('Y-m-d h:i:s')
+                );
+                $this->db->insert('tbl_flyer_tags', $datas);
+                $data['sucess'] = 'Flyer Added successfully';
             }
         }
         $this->commonmodel->adminloadLayout($data, 'admin/manageflyers/add_flyer_tags');
     }
+
     public function add_button_tags() {
 
-        $data  = '';
+        $data = '';
         if (isset($_POST['submit'])) {
             $this->form_validation->set_rules('button_tags_title', 'Button Title', 'required');
             if ($this->form_validation->run() == FALSE) {
-            
                 
             } else { // no errors now to save the data
-                $button_tags_title        = $this->input->post('button_tags_title');
+                $button_tags_title = $this->input->post('button_tags_title');
                 $datas = array(
-                        'button_tags_title' => $button_tags_title,
-                        'button_tags_date' => date('Y-m-d h:i:s')
-                    );
-                     $this->db->insert('tbl_button_tags', $datas);
-                
-                $data['sucess']  = 'Button Added successfully';
+                    'button_tags_title' => $button_tags_title,
+                    'button_tags_date' => date('Y-m-d h:i:s')
+                );
+                $this->db->insert('tbl_button_tags', $datas);
+
+                $data['sucess'] = 'Button Added successfully';
             }
         }
         $this->commonmodel->adminloadLayout($data, 'admin/manageflyers/add_button_tags');
     }
-   
-    
+
 }
--- application/models/admin/flyersmodel.php
+++ application/models/admin/flyersmodel.php
@@ -30,6 +30,7 @@ class flyersmodel extends CI_Model {
             return '';
         }
     }
+
     public function getAllflyer_tags() {
         $queryStr = " SELECT * FROM tbl_flyer_tags order by flyer_tags_date DESC";
         $queryRS = $this->db->query($queryStr);
@@ -39,6 +40,7 @@ class flyersmodel extends CI_Model {
             return '';
         }
     }
+
     public function getAllbutton_tags() {
         $queryStr = " SELECT * FROM tbl_button_tags order by button_tags_date DESC ";
         $queryRS = $this->db->query($queryStr);
@@ -49,5 +51,48 @@ class flyersmodel extends CI_Model {
         }
     }
 
-  
-}//end of class
+    public function getAllFlyerSize($status) {
+        $query = $this->db->get_where('tbl_flyer_size', array('flyer_size_status' => $status));
+
+        $flyerSize = $query->result_array();
+
+        return count($flyerSize) > 0 ? $flyerSize : FALSE;
+    }
+
+    public function saveFlyer($data) {
+        $query = "INSERT INTO tbl_flyer_detail (flyer_image, flyer_image_size, flyer_creation_date)  VALUES (:image, :sizeid, NOW())";
+
+        $statment = $this->db->conn_id->prepare($query);
+        $statment->bindParam(':image', $data['image'], PDO::PARAM_STR);
+        $statment->bindParam(':sizeid', $data['sizeid'], PDO::PARAM_INT);
+
+        return $statment->execute() ? $this->db->conn_id->lastInsertId() : FALSE;
+    }
+
+    public function saveFlyerBtnRelation($data) {
+        foreach ($data['btn'] as $key => $value) {
+            $query = "INSERT INTO tbl_r_flyer_btn_tag (fk_flyer_id, fk_btn_tag_id) VALUES (:flyerid, :btnid)";
+
+            $statment = $this->db->conn_id->prepare($query);
+            $statment->bindParam(':flyerid', $data['flyerid'], PDO::PARAM_INT);
+            $statment->bindParam(':btnid', $value, PDO::PARAM_INT);
+            $statment->execute();
+        }
+        return;
+    }
+
+    public function saveFlyerFlyertagsRelation($data) {
+        foreach ($data['flyer'] as $key => $value) {
+            $query = "INSERT INTO tbl_r_flyer_flyer_tag (fk_flyer_id, fk_flyer_tag_id) VALUES (:flyerid, :flyertagid);";
+
+            $statment = $this->db->conn_id->prepare($query);
+            $statment->bindParam(':flyerid', $data['flyerid'], PDO::PARAM_INT);
+            $statment->bindParam(':flyertagid', $value, PDO::PARAM_INT);
+            $statment->execute();
+        }
+        return;
+    }
+
+}
+
+//end of class
